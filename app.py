c1a93e3527dbb158217067e04961e26c5da23b71
Thor W <6273158@rts-365.dk>
2021-11-05 17:52:01 +0100

    initialized discord, and created template events

--- app.py
+++ app.py
@@ -2,16 +2,32 @@
 # A Client is an object that represents a connection to Discord. A Client handles events, tracks state, and generally interacts with Discord APIs.
 
 import os
+
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    # guild = discord.utils.get(client.guilds, name=GUILD)
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.content == "sindri": 
+        await message.channel.send("er en faggot ez")
+
+    if message.content == "/help":
+        await message.channel.send("nej")
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hej {member.name}, Velkommen til serveren!')
 
 client.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Posture bot for Discord
