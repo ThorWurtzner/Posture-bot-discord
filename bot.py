5b91e2843ab0364ebb82a82deb1c548b55d5a37d
Thor W <6273158@rts-365.dk>
2021-11-10 13:37:14 +0100

    changed interval to 3s and added protection against abuse from malthe

--- bot.py
+++ bot.py
@@ -17,16 +17,16 @@ async def play_source(ctx):
     # put below for local use -> executable="C:/ffmpeg/ffmpeg.exe"
     source = FFmpegPCMAudio(source='sound.mp3')
     ctx.voice_client.play(source, after=lambda e: bot.loop.create_task(play_source(ctx)))
-    time.sleep(60 * 30)
+    time.sleep(3)
 
 @bot.command()
 async def posture(ctx):
-    if ctx.author.voice:
+    if ctx.author.voice and not ctx.voice_client:
         await ctx.reply(f"Joining {ctx.author.name}'s channel ☜(ﾟヮﾟ☜)")
         await ctx.author.voice.channel.connect()
 
         bot.loop.create_task(play_source(ctx))
-        time.sleep(60 * 30)
+        time.sleep(3)
 
     else:
         await ctx.reply("You must be in a voice channel.")
