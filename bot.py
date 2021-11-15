45de626f3f7939c6c93d9b600f0ec3481b3dbffd
Thor W <6273158@rts-365.dk>
2021-11-15 12:40:57 +0100

    changed sleep to async

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 
 import os
-import time
+import asyncio
 
 from discord.ext import commands
 from discord import FFmpegPCMAudio
@@ -17,7 +17,7 @@ async def play_source(ctx):
     # put below for local use -> executable="C:/ffmpeg/ffmpeg.exe"
     source = FFmpegPCMAudio(source='sound.mp3')
     ctx.voice_client.play(source, after=lambda e: bot.loop.create_task(play_source(ctx)))
-    time.sleep(20)
+    await asyncio.sleep(900)
 
 @bot.command()
 async def posture(ctx):
