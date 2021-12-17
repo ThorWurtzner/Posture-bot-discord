7261516a4280c76e971d19e37adbde825232b895
Thor W <6273158@rts-365.dk>
2021-12-17 16:07:09 +0100

    added amogus

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import os
 import asyncio
 
 from discord.ext import commands
-from discord import FFmpegPCMAudio
+from discord import FFmpegPCMAudio, voice_client
+from discord import Client;
 
 from dotenv import load_dotenv
 
@@ -12,6 +13,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix="-")
+# client = Client();
 
 async def play_source(ctx):
     # put below for local use -> executable="C:/ffmpeg/ffmpeg.exe"
@@ -42,17 +44,17 @@ async def suh(ctx):
         await ctx.send("I am not in a voice channel.")
 
 @bot.command()
-async def dude(ctx):
+async def boi(ctx):
     if ctx.voice_client:
-        await ctx.reply("suh")
+        islandboy = FFmpegPCMAudio(source='islandboy.mp3')
+        await ctx.voice_client.play(islandboy)
     else:
         await ctx.send("I am not in a voice channel.")
 
 @bot.command()
-async def boi(ctx):
+async def dude(ctx):
     if ctx.voice_client:
-        songClip = FFmpegPCMAudio(source='islandboy.mp3')
-        await ctx.voice_client.play(songClip)
+        await ctx.reply("suh")
     else:
         await ctx.send("I am not in a voice channel.")
 
@@ -63,5 +65,20 @@ async def leave(ctx):
         await ctx.reply("I'm out, cya!")
     else:
         await ctx.send("I am not in a voice channel.")
+
+@bot.command()
+async def sus(ctx):
+    if ctx.voice_client:
+        amogus = FFmpegPCMAudio(source='amogus.mp3')
+        await ctx.voice_client.play(amogus)
+    else:
+        await ctx.send("I am not in a voice channel.")
+
+# async def check():
+#     memberCount = len(client.voice_channel.members)
+#     if memberCount == 1:
+#         client.voice_client.disconnect()
+#     await asyncio.sleep(60)
+# client.loop.create_task(check())
 
 bot.run(TOKEN)
